6573fdef808b830f6f6e70a60e358ce2dcda80d1
reza golestany <golestany@email.kntu.ac.ir>
2024-10-25 11:12:00 +0330

    enumerations

---
+++ src/entity/enums/Department.java
@@ -0,0 +1,5 @@
+package entity.enums;
+
+public enum Department {
+    HR, REGISTER, ENGINEERING, MEDICAL_SCIENCE, AGRICULTURAL, FINANCE
+}
---
+++ src/entity/enums/StaffPosition.java
@@ -0,0 +1,5 @@
+package entity.enums;
+
+public enum StaffPosition {
+    MANAGER, ACCOUNTANT, SECRETARY, SECURITY ,SPECIALIST
+}
