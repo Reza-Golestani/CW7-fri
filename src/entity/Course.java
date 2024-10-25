c3c8efb8238e050f802a56148b700addbfc29cde
reza golestany <golestany@email.kntu.ac.ir>
2024-10-25 11:09:17 +0330

    course class and service

---
+++ src/entity/Course.java
@@ -0,0 +1,15 @@
+package entity;
+
+public class Course {
+
+    private Teacher teacher;
+
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+}
