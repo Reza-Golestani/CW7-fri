02b03887f767f0bfd50d72a78f2570e6dd542dab
reza golestany <golestany@email.kntu.ac.ir>
2024-10-25 11:05:33 +0330

    local database

---
+++ src/database/Database.java
@@ -0,0 +1,14 @@
+package database;
+
+import entity.User;
+import entity.UserList;
+
+import java.util.ArrayList;
+
+public class Database {
+
+    public static User loggedInUser;
+
+    public static UserList users = new UserList();
+
+}
